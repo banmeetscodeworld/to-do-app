d9897535f5d63c1e7d2d6e95f3b788ba3a79b9f4
Banmeet Kaur <103919531+banmeetscodeworld@users.noreply.github.com>
2024-04-14 21:36:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ const rootReducer = combineReducers({
 });
 
 export default rootReducer;
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
